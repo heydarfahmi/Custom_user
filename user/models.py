d43b4fc64d4ac913e244669cc3d4f5bda3ff0b5e
Heydar Fahmi <HeydarFahmi2000@gmail.com>
2020-02-08 17:29:59 +0330

    add fields to django.user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class CustomUserExtraField(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField(blank=False, null=False)
+    phoneNumber = models.PositiveIntegerField()
+    postalCode = models.PositiveIntegerField()
+
+    Bio = models.TextField(null=True, blank=True, default="Bio")
