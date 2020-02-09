cec79329b2d97c66a6e8f011696ef6b91165b6e4
Heydar Fahmi <HeydarFahmi2000@gmail.com>
2020-02-09 10:23:22 +0330

    register and customize admin panel user

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from user.models import CustomUser
+
+
+class CustomUserAdminPanel(admin.ModelAdmin):
+    list_display = ('username','first_name', 'last_name',)
+    list_filter = ('is_staff','is_superuser','last_login',)
+    list_per_page = 20
+    exclude = ('last_login')
+admin.site.register(CustomUser,CustomUserAdminPanel)
 
 # Register your models here.
